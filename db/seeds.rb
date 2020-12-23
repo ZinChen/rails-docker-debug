db0efd54e470b825470b8be0cae3eed21780c0b4
Y.Zinchenko <zinchen777@gmail.com>
2020-12-23 10:49:49 +0300

    add Books scaffold

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Book.create(title: "War and Peace", author: "Leo Tolstoy", publication_year: 1869)
+Book.create(title: "Hamlet", author: "William Shakespeare", publication_year: 1870)
+Book.create(title: "Crime and Punishment", author: "Fyodor Dostoevsky", publication_year: 1866)
